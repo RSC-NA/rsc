bcbfee598a9ac5008e0358fec94509558cca4fa5
xentrick <itsnick@gmail.com>
2025-08-04 15:41:14 -0400

    Intent by Franchise cmd

--- rsc/admin/stats.py
+++ rsc/admin/stats.py
@@ -102,6 +102,87 @@ class AdminStatsMixIn(AdminMixIn):
 
         await interaction.followup.send(embed=embed)
 
+    @_stats.command(name="intentbyfranchise", description="Intent to Play statistics by Franchise")  # type: ignore[type-var]
+    async def _intent_stats_franchise_cmd(self, interaction: discord.Interaction):
+        guild = interaction.guild
+        if not guild:
+            return
+        if not isinstance(interaction.user, discord.Member):
+            return
+
+        await interaction.response.defer(ephemeral=True)
+
+        try:
+            next_season = await self.next_season(guild)
+        except LeagueNotConfigured:
+            return await interaction.followup.send(
+                embed=YellowEmbed(
+                    title="Not Configured",
+                    description="League ID has not been configured for this guild.",
+                )
+            )
+        except RscException as exc:
+            return await interaction.followup.send(embed=ApiExceptionErrorEmbed(exc))
+
+        if not next_season:
+            return await interaction.followup.send(
+                embed=YellowEmbed(
+                    title="Intent To Play Statistics",
+                    description="The next season of RSC has not started yet.",
+                )
+            )
+
+        if not next_season.id:
+            return await interaction.followup.send(
+                embed=ErrorEmbed(description="API returned a Season without an ID. Please open a modmail ticket.")
+            )
+
+        log.debug(f"Next Season ID: {next_season.id}")
+        intents = await self.player_intents(guild, season_id=next_season.id)
+
+        log.debug(f"Intent Count: {len(intents)}")
+        if not intents:
+            return await interaction.followup.send(
+                embed=YellowEmbed(
+                    title="Intent Statistics",
+                    description="There are no intents declared for next season.",
+                )
+            )
+
+        franchise_intents: dict[str, dict[str, int]] = {}
+
+        log.debug(f"Intents[0]: {intents[0] if intents else 'No intents'}")
+
+        for i in intents:
+            if not i.player:
+                log.warning(f"Intent without player: {i}")
+                continue
+
+            if not i.player.franchise:
+                continue
+
+            franchise_intents.setdefault(i.player.franchise, {"Completed": 0, "Missing": 0, "Total": 0})
+
+            if i.missing:
+                franchise_intents[i.player.franchise]["Missing"] += 1
+            else:
+                franchise_intents[i.player.franchise]["Completed"] += 1
+
+            franchise_intents[i.player.franchise]["Total"] += 1
+
+        sorted_intents = dict(sorted(franchise_intents.items(), key=lambda x: x[1]["Missing"], reverse=True))
+
+        embed = BlueEmbed(
+            title="Intent to Play Franchise Statistics",
+            description=f"Intent to Play statistics by franchise for S{next_season.number}",
+        )
+
+        embed.add_field(name="Franchise", value="\n".join(sorted_intents.keys()), inline=True)
+        embed.add_field(name="Completed", value="\n".join([str(f["Completed"]) for f in sorted_intents.values()]), inline=True)
+        embed.add_field(name="Missing", value="\n".join([str(f["Missing"]) for f in sorted_intents.values()]), inline=True)
+
+        await interaction.followup.send(embed=embed)
+
     @_stats.command(name="current", description="Current season statistics")  # type: ignore[type-var]
     async def _current_season_stats_cmd(self, interaction: discord.Interaction):
         guild = interaction.guild
